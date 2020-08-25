e8908dc56887c618815a3d5db3e5c45611bb2d61
lewan42 <lewanov888@gmail.com>
2020-08-25 23:11:18 +0500

    test

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -3,12 +3,23 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-
 }
